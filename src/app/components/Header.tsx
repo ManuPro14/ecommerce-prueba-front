36b61ac11d46561db72434940ea4c61e24a57d07
Manuel <manuelcar96@outlook.com>
2025-05-17 11:48:51 -0500

    feat: se crea ruta del login para admin

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,9 +6,14 @@ function Header() {
   return (
     <header className='flex justify-between items-center h-20 bg-gray-900'>
       <h1 className='text-purple-400 text-4xl font-bold border-b-2 border-purple-400 mb-4 ml-8'>Ecommerce</h1>
-      <button onClick={() => window.location.href = '/products'} className='bg-purple-600 hover:bg-purple-500/80 hover:scale-105 text-white px-6 py-2 rounded-full text-sm font-semibold mr-8 flex items-center cursor-pointer  transition duration-300 ease-in-out'>
-        <span className='text-white text-lg font-semibold'>Go to Products</span>
-      </button>
+      <div className='flex items-center'>
+        <button onClick={() => window.location.href = '/admin'} className='hover:scale-105 text-white px-6 py-2 rounded-full text-sm font-semibold mr-8 flex items-center cursor-pointer  transition duration-300 ease-in-out'>
+          <span className='text-purple-400 text-xl font-semibold'>Login</span>
+        </button>  
+        <button onClick={() => window.location.href = '/products'} className='bg-purple-600 hover:bg-purple-500/80 hover:scale-105 text-white px-6 py-2 rounded-full text-sm font-semibold mr-8 flex items-center cursor-pointer  transition duration-300 ease-in-out'>
+          <span className='text-white text-lg font-semibold'>Go to Products</span>
+        </button>
+      </div>
     </header>
   )
 }
